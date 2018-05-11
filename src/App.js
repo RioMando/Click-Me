eced8f6c13b28164798199683d948441e8a92218
RioMando <roblesrioarmand@gmail.com>
2018-05-10 22:34:47 -0500

    Add file images.json, add background image

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React from 'react';
-// import logo from './logo.svg';
-import './App.css';
-import Images from "./components/Images";
+
+import ImageCard from "./components/ImageCard";
 import Navbar from './components/Navbar';
 import Jumbotron from './components/Jumbotron';
+import images from './images.json';
 
 
 const App = () => (
   <div className="container">
     <Navbar />
     <Jumbotron />
-    <Images />
     
+    <ImageCard image={images[0].image} />
+    <ImageCard image={images[1].image} />
+    <ImageCard image={images[2].image} />
+    <ImageCard image={images[3].image} />
+    <ImageCard image={images[4].image} />
+    <ImageCard image={images[5].image} />
+    <ImageCard image={images[6].image} />
+    <ImageCard image={images[7].image} />
+    <ImageCard image={images[8].image} />
+    <ImageCard image={images[9].image} />
+    <ImageCard image={images[10].image} />
+    <ImageCard image={images[11].image} />
   </div>
+      
 );
 
 export default App;
